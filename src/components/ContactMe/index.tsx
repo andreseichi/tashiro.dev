88752434672c5e8af5d85a2794d877f95fd1cc0d
tashiro <andre_seichi@hotmail.com>
2023-01-31 22:14:06 -0300

    feat: better mobile responsive contact

--- src/components/ContactMe/index.tsx
+++ src/components/ContactMe/index.tsx
@@ -26,43 +26,45 @@ export function ContactMe() {
   };
 
   return (
-    <div className="h-screen flex flex-col relative justify-evenly items-center text-center md:text-left md:flex-row max-w-7xl px-10 mx-auto">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+    <div className="min-h-screen flex flex-col relative justify-evenly items-center text-center md:text-left md:flex-row max-w-7xl px-3 md:px-10 mx-auto">
+      <h3 className="absolute top-14 hidden md:inline uppercase tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
 
-      <div className="flex flex-col gap-10">
-        <h4 className="text-4xl font-semibold text-center">
+      <div className="flex flex-col gap-8 md:gap-10 w-full mx-auto">
+        <h4 className="text-2xl md:text-4xl font-semibold text-center">
           Contact me for more information.{' '}
-          <span className="decoration-primary underline">Lets talk!</span>
+          <span className="decoration-primary underline whitespace-nowrap">
+            Lets talk!
+          </span>
         </h4>
 
-        <div className="flex flex-col justify-center items-center gap-6">
-          <div className="flex items-center justify-center gap-5">
+        <div className="flex flex-col justify-center items-center gap-2 md:gap-6">
+          <div className="flex items-center justify-center gap-4">
             <Phone
               size={32}
               weight="fill"
               className="text-primary animate-pulse"
             />
-            <p className="text-2xl">+5591980448461</p>
+            <p className="text-xl md:text-2xl">+5591980448461</p>
           </div>
 
-          <div className="flex items-center justify-center gap-5">
+          <div className="flex items-center justify-center gap-4">
             <MapPin
               size={32}
               weight="fill"
               className="text-primary animate-pulse"
             />
-            <p className="text-2xl">Pará - Brazil</p>
+            <p className="text-xl md:text-2xl">Pará - Brazil</p>
           </div>
 
-          <div className="flex items-center justify-center gap-5">
+          <div className="flex items-center justify-center gap-4">
             <EnvelopeSimple
               size={32}
               weight="duotone"
               className="text-primary animate-pulse"
             />
-            <p className="text-2xl">tashiro.dev@gmail.com</p>
+            <p className="text-xl md:text-2xl">tashiro.dev@gmail.com</p>
           </div>
         </div>
 
@@ -70,17 +72,17 @@ export function ContactMe() {
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col gap-2 w-fit mx-auto"
         >
-          <div className="flex gap-2">
+          <div className="flex gap-2 flex-wrap">
             <input
               {...register('name')}
               placeholder="Name"
-              className="contactInput"
+              className="contactInput flex-1"
               type="text"
             />
             <input
               {...register('email')}
               placeholder="Email"
-              className="contactInput"
+              className="contactInput flex-1"
               type="email"
             />
           </div>
@@ -100,7 +102,7 @@ export function ContactMe() {
           />
           <button
             type="submit"
-            className="bg-primary py-5 px-10 rounded-md text-black font-bold text-lg"
+            className="bg-primary py-4 px-10 rounded-md text-black font-bold text-lg"
           >
             Submit
           </button>
