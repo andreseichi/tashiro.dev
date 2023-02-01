90b9293f5961390e3b3bbf7529cd9180ab9d440e
tashiro <andre_seichi@hotmail.com>
2023-01-31 21:36:39 -0300

    feat: better mobile responsive for projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,9 +1,12 @@
-import { motion } from 'framer-motion';
 import Image from 'next/image';
+import Link from 'next/link';
+
+import { motion } from 'framer-motion';
+
+import { TechnologyIcon } from '../TechnologyIcon';
+import data from '@/data/projects.json';
 
 export function Projects() {
-  const projects = [1, 2, 3, 4, 5];
-
   return (
     <motion.div
       initial={{
@@ -13,60 +16,69 @@ export function Projects() {
         opacity: 1,
       }}
       transition={{ duration: 1.5 }}
-      className="h-screen flex flex-col justify-evenly items-center text-left md:flex-row relative overflow-hidden max-w-full px-10 mx-auto z-0"
+      className="min-h-screen flex flex-col justify-evenly items-center text-left md:flex-row relative overflow-hidden max-w-full px-3 md:px-10 mx-auto z-0"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-14 hidden md:inline md:top-20 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
 
-      <div className="relative overflow-x-scroll overflow-y-hidden w-full flex snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-primary">
-        {projects.map((project, index) => (
-          <div
+      <div className="w-full flex space-x-5 overflow-x-scroll pb-10 md:p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-primary z-10">
+        {data.projects.map((project, index) => (
+          <Link
             key={index}
-            className="w-screen flex flex-col gap-5 items-center justify-center p-20 md:p-44 flex-shrink-0 snap-center h-screen"
+            href={project.url}
+            target="_blank"
+            className=" relative rounded-xl flex-shrink-0 w-full md:w-[600px] xl:w-[900px] snap-center bg-gradient-to-r p-[5px] from-[#7928ca] to-[#ff0080] cursor-pointer transition-opacity duration-200 overflow-hidden"
           >
-            <motion.div
-              initial={{
-                y: -300,
-                opacity: 0,
-              }}
-              transition={{
-                duration: 1.2,
-              }}
-              whileInView={{
-                y: 0,
-                opacity: 1,
-              }}
-              viewport={{ once: true }}
-              className="relative object-cover flex-shrink-0 max-w-[320px] max-h-[320px]
-              xl:max-w-[420px] xl:max-h-[420px] overflow-hidden"
-            >
-              <Image
-                src="/profile.jpg"
-                alt="project"
-                width={1280}
-                height={1280}
-              />
-            </motion.div>
+            <div className="flex flex-col gap-7 items-center rounded-xl w-full h-full bg-black p-4">
+              <motion.div
+                initial={{
+                  y: 100,
+                  opacity: 0,
+                }}
+                transition={{
+                  duration: 1.2,
+                }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                }}
+                viewport={{ once: true }}
+                className="w-28 h-28 xl:w-[140px] xl:h-[140px] object-cover object-center overflow-hidden"
+              >
+                <Image
+                  src={project.image}
+                  alt="experience"
+                  width={1280}
+                  height={1280}
+                />
+              </motion.div>
 
-            <div className="gap-10 px-0 md:px-10 max-w-6xl flex flex-col">
-              <h4 className="text-4xl font-semibold text-center">
-                <span className="underline decoration-primary">Project</span>{' '}
-                something
-              </h4>
+              <h4 className="tracking-widest text-xl font-bold">Projeto X</h4>
 
-              <p className="text-lg text-center md:text-left">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Maiores error, placeat reprehenderit, veritatis molestias
-                sapiente rerum perferendis quia quidem quos, nulla sint quis
-                autem! Aliquid architecto obcaecati possimus natus labore.
+              <div className="flex space-x-2 my-2">
+                {project.technologies.map((technology, index) => (
+                  <TechnologyIcon
+                    id={technology.id}
+                    imageSrc={technology.imageSrc}
+                    name={technology.name}
+                    key={index}
+                  />
+                ))}
+              </div>
+
+              <p className="text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repudiandae possimus tenetur maiores suscipit ipsa quo nostrum
+                ratione consequuntur optio accusamus? Sunt, modi enim. Officia
+                id dicta dolorum neque tempore doloremque.
               </p>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
 
-      <div className="w-full absolute top-[30%] bg-primary/20 left-0 h-[500px] skew-y-12"></div>
+      <div className="w-full absolute top-[30%] bg-primary/20 left-0 h-[500px] skew-y-12 z-0"></div>
     </motion.div>
   );
 }
